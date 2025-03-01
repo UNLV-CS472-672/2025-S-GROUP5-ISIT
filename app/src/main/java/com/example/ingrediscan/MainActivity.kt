f66e51e9c0087e83fadc76c532cae4392da13d92
AlanReisenau <afreisenauer@gmail.com>
2025-03-01 13:05:38 -0800

    This is the addition of the generic app structure.

---
+++ app/src/main/java/com/example/ingrediscan/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.ingrediscan
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.ingrediscan.ui.theme.IngrediScanTheme
+
+//generate the highest level of the front page & other elements
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            IngrediScanTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+//Runs a basic display text box on the front page
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+//generates elements to go into the front page of the app
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    IngrediScanTheme {
+        Greeting("Android")
+    }
+}
