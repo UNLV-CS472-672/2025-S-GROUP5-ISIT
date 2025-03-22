8e15d8a2be567575e82f6959b55e35cece4e5ab0
JaisonNoah808 <146907165+JaisonNoah808@users.noreply.github.com>
2025-03-22 14:15:24 -0700

    This is for the Scan page to take pictures of the item/barcode. Later functionality and positioning will be done.

--- app/src/main/java/com/example/ingrediscan/ui/search/SearchFragment.kt
+++ app/src/main/java/com/example/ingrediscan/ui/search/SearchFragment.kt
@@ -28,10 +28,10 @@ class SearchFragment : Fragment() {
         _binding = FragmentScanBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textScan
-        scanViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+//        val textView: TextView = binding.textScan
+//        scanViewModel.text.observe(viewLifecycleOwner) {
+//            textView.text = it
+//        }
         return root
     }
 
