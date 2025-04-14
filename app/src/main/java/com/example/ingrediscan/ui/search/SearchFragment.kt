f467e8f19fb3b8a74b61e33ef475c89a87209c66
riley-ramos <115125320+riley-ramos@users.noreply.github.com>
2025-04-14 00:01:46 -0700

    Updated search page UI and functionality
    Updated search page and recommended titles, added outline to search bar, increased space between title and recommended title, added "Read More" buttons and a pie chart displaying the macro nutrients to each search result, and added a search result data class.

--- app/src/main/java/com/example/ingrediscan/ui/search/SearchFragment.kt
+++ app/src/main/java/com/example/ingrediscan/ui/search/SearchFragment.kt
@@ -1,5 +1,6 @@
 package com.example.ingrediscan.ui.search
 
+import com.example.ingrediscan.R 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -10,6 +11,11 @@ import android.util.Log
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.ingrediscan.databinding.FragmentSearchBinding
+import com.github.mikephil.charting.charts.PieChart
+import com.github.mikephil.charting.data.PieData
+import com.github.mikephil.charting.data.PieDataSet
+import com.github.mikephil.charting.data.PieEntry
+import androidx.core.content.ContextCompat
 
 class SearchFragment : Fragment() {
 
@@ -31,7 +37,7 @@ class SearchFragment : Fragment() {
         val root: View = binding.root
         val searchTitleTextView: TextView = binding.searchTitle
 
-        searchTitleTextView.text = "Search"
+        searchTitleTextView.text = "Need meal inspiration?"
 
         return root
     }
@@ -57,6 +63,16 @@ class SearchFragment : Fragment() {
                 return false
             }
         })
+        
+        // Create test items
+        val salad = SearchResult(1, "Caesar Salad", 200, 7, 10, 18, "B-", "A classic Caesar salad offers crisp romaine lettuce tossed with creamy dressing, crunchy croutons, and a sprinkle of parmesan. While it's flavorful and has some fiber and calcium, it can be high in fat and sodium without much protein unless you add chicken or another topping.")
+        salad.makePieChart(requireContext(), binding.pieChart)
+
+        val blt = SearchResult(2, "BLT Sandwich", 500, 13, 30, 25, "C+", "A BLT is a satisfying sandwich with crispy bacon, fresh lettuce, and juicy tomato layered between toasted bread and a swipe of mayo. It's tasty and filling but relatively high in fat and sodium, and not especially nutrient-dense.")
+        blt.makePieChart(requireContext(), binding.pieChart2)
+
+        val spaghetti = SearchResult(3, "Spaghetti", 500, 10, 60, 4, "B", "Spaghetti with marinara is a simple, comforting dish made mostly of pasta and tomato-based sauce. It’s low in fat and can be a decent source of fiber and vitamins if whole wheat pasta or extra veggies are added, but it's fairly carb-heavy.")
+        spaghetti.makePieChart(requireContext(), binding.pieChart3)
     }
 
     // Future TODO: Implement search logic 
